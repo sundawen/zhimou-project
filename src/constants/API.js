313c7f63bf9913101c5c92f97ba1d4dd9cb21ccb
Dawen Sun <sundw4@lenovo.com>
2019-05-24 17:10:57 +0800

    update chart three

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,3 +1,3 @@
 // export const API_CHANNEL = '/start?camId=1'
 export const API_STREAM_CHANNELONE = '/stream'
-export const API_CHART = '/stream/chart'
+export const API_CHART = '/HistoryError/today'
--- src/containers/Video/Video.jsx
+++ src/containers/Video/Video.jsx
@@ -59,7 +59,7 @@ class Video extends React.Component {
         channel: '',
       },
       chartData: [],
-      chartTotal: 33,
+      chartTotal: 0,
     }
   }
   componentWillMount = () => {
@@ -75,6 +75,7 @@ class Video extends React.Component {
       })
       this.setState({ box2: this.state.nullList }, () => {
       })
+      this.getCurrentInfo();
       // if (this.props.channelList !== nextProps.channelList) {
       //   this.setState({ box1: this.props.channelList }, () => {
       //   })
@@ -84,70 +85,83 @@ class Video extends React.Component {
       })
       this.setState({ box1: this.state.nullList }, () => {
       })
+      this.getCurrentInfo();
     } else {
       this.setState({ box1: this.state.nullList }, () => {
       })
       this.setState({ box2: this.state.nullList }, () => {
       })
     }
-
-    if (this.props.videoBox1Data.info.ErrorType) {
-      let errorType = this.props.videoBox1Data.info.ErrorType;
-      if (errorType == 'BlueScreen') {
-        this.state.chartData[0].count++;
-      } else if (errorType == 'Smear') {
-        this.state.chartData[1].count++;
-      } else if (errorType == 'Tortuosity') {
-        this.state.chartData[2].count++;
-      }
-    }
   }
 
   getCurrentDateData() {
-    const { cameraId } = this.state;
     let date = new Date();
     let mouth = date.getMonth() + 1;
-    let time = date.getFullYear() + mouth + date.getDate();
-    let params = '?camId=' + cameraId + '&time=' + time;
-    fetch(API_CHART + params).then(res => res.json()).then(data => {
-      pagination.total = parseInt(data.totalnum);
-      data.totalinfo.forEach((item, key) => {
-        item.key = key;
-      });
-      this.setState({
-        detailsData: data.totalinfo,
-        pagination
-      });
-      loading.table = false;
-      this.setState({ loading });
-    }).catch(err => {
-      // 测试代码数据
-      console.log('测试数据');
-      // let data = {"totalnum":"47","info":[{"blueScreen":"18","smear":"6","tortuosity":"23"}]};
-      const data = [
+    let time = date.getFullYear() + '-' + mouth + '-' + date.getDate();
+    let params = '?date=' + time;
+    fetch(API_CHART + params).then(res => res.json()).then(json => {
+      let data = [
         {
           item: zh_CN.blueScreen,
-          count: 1
+          count: json.info[0].blueScreen
         },
         {
           item: zh_CN.smear,
-          count: 2
+          count: json.info[0].smear
         },
         {
           item: zh_CN.tortuosity,
-          count: 3
+          count: json.info[0].tortuosity
         }
       ];
       this.setState({
         chartData: data,
+        chartTotal: json.totalnum,
+      });
+    }).catch(err => {
+      // 测试代码数据
+      console.log('测试数据');
+      let json = {"totalnum":30,"info":{"blueScreen":10,"smear":5,"tortuosity":15}};
+      let data = [
+        {
+          item: zh_CN.blueScreen,
+          count: json.info.blueScreen
+        },
+        {
+          item: zh_CN.smear,
+          count: json.info.smear
+        },
+        {
+          item: zh_CN.tortuosity,
+          count: json.info.tortuosity
+        }
+      ];
+      this.setState({
+        chartData: data,
+        chartTotal: json.totalnum,
       });
     })
   }
 
+  getCurrentInfo() {
+    let errorType = this.props.channelList.info.ErrorType;
+    if (errorType == 'BlueScreen') {
+      this.state.chartData[0].count++;
+      this.state.chartTotal++;
+    } else if (errorType == 'Smear') {
+      this.state.chartData[1].count++;
+      this.state.chartTotal++;
+    } else if (errorType == 'Tortuosity') {
+      this.state.chartData[2].count++;
+      this.state.chartTotal++;
+    }
+  }
+
   render() {
-    const { chartData, chartTotal } = this.state;
+    const { chartData } = this.state;
     const { Html } = Guide;
-    const dv = new DataView()
+
+    const dv = new DataView();
     dv.source(chartData).transform({
       type: "percent",
       field: "count",
@@ -162,6 +176,35 @@ class Video extends React.Component {
         }
       }
     };
+
+    const ds = new DataSet();
+    const cv = ds.createView().source(chartData);
+    cv.source(chartData).transform({
+      type: "sort",
+
+      callback(a, b) {
+        // 排序依据，和原生js的排序callback一致
+        return a.count - b.count > 0;
+      }
+    });
+
+    const dc = new DataView().source(chartData);
+    dc.transform({
+      type: "fold",
+      fields: ["count"],
+      // 展开字段集
+      key: "user",
+      // key字段
+      value: "score" // value字段
+    });
+
+    const colsc = {
+      score: {
+        min: 0,
+        max: 80
+      }
+    };
+
     return (
       <div>
         <Header />
@@ -172,10 +215,10 @@ class Video extends React.Component {
           </Col>
           <Col xl={6} lg={24} md={24} sm={24} xs={24}>
             <Chart
-              height={350}
+              height={300}
               data={dv}
               scale={cols}
-              padding={[70, 50, 20, 50]}
+              padding={[70, 20, 20, 20]}
               forceFit
             >
               <Coord type={"theta"} radius={0.9} innerRadius={0.8} />
@@ -192,7 +235,7 @@ class Video extends React.Component {
               <Guide>
                 <Html
                   position={["50%", "50%"]}
-                  html={() => { return ('<div style=color:white;font-size:1.16em;text-align:center;width: 10em;>当日监控一故障统计<br><span style=color:white;font-size:2.5em>' + this.state.chartTotal + '</span>台') }}
+                  html={() => { return ('<div style=color:white;font-size:1.16em;text-align:center;width: 10em;>当日故障统计<br><span style=color:white;font-size:2.5em>' + this.state.chartTotal + '</span>台') }}
                   alignX="middle"
                   alignY="middle"
                 />
@@ -223,6 +266,64 @@ class Video extends React.Component {
                   }}
                 />
               </Geom>
+            </Chart>
+            <Chart height={300} data={cv} forceFit>
+              <Coord transpose />
+              <Axis
+                name="item"
+                label={{
+                  offset: 12
+                }}
+              />
+              <Axis name="count" />
+              <Tooltip />
+              <Geom type="interval" position="item*count" />
+            </Chart>
+            <Chart
+              height={300}
+              data={dc}
+              padding={[0, 0, 0, 0]}
+              scale={colsc}
+              forceFit
+            >
+              <Coord type="polar" radius={0.8} />
+              <Axis
+                name="item"
+                line={null}
+                tickLine={null}
+                grid={{
+                  lineStyle: {
+                    lineDash: null
+                  },
+                  hideFirstLine: false
+                }}
+              />
+              <Tooltip />
+              <Axis
+                name="score"
+                line={null}
+                tickLine={null}
+                grid={{
+                  type: "polygon",
+                  lineStyle: {
+                    lineDash: null
+                  },
+                  alternateColor: "rgba(0, 0, 0, 0.04)"
+                }}
+              />
+              <Geom type="line" position="item*score" color="user" size={2} />
+              <Geom
+                type="point"
+                position="item*score"
+                color="user"
+                shape="circle"
+                size={4}
+                style={{
+                  stroke: "#fff",
+                  lineWidth: 1,
+                  fillOpacity: 1
+                }}
+              />
             </Chart>
           </Col>
         </Row>
